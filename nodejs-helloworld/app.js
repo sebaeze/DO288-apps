ae608130edac94a76d0c6332d936db887a05c181
Sebastian Andreoletti <andreole@ar.ibm.com>
2022-03-06 09:17:02 -0300

    variable ent

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 app = express();
-
+//
+console.log("...estoy en app.js \n **** env: "+process.env.TEST||"_NO_VAR_ENTORNO_"+"***\n****");
+//
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
