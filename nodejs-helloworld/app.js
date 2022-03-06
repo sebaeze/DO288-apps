146ccedce9d8af16d1e4f0c3c5329eaeaf649f59
Sebastian Andreoletti <andreole@ar.ibm.com>
2022-03-06 09:19:12 -0300

    file vol

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,7 +1,17 @@
 var express = require('express');
+var fs = require('fs') ;
+//
 app = express();
 //
 console.log("...estoy en app.js \n **** env: "+process.env.TEST||"_NO_VAR_ENTORNO_"+"***\n****");
+//
+fs.readFile('/config.env', 'utf8' , (err, data) => {
+	  if (err) {
+		      console.error(err)
+		      return
+		    }
+  console.log("....config.env: "+data+";") ;
+}) ;
 //
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
