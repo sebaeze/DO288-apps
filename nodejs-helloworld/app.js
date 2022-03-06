837b7cb1a03957595b9942b1d6a8442837fb7ed2
Sebastian Andreoletti <andreole@ar.ibm.com>
2022-03-06 09:46:13 -0300

    fff

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -5,8 +5,11 @@ app = express();
 //
 console.log("...estoy en app.js \n **** env: "+process.env.TEST||"_NO_VAR_ENTORNO_"+"***\n****");
 //
-fs.readFile('/config.env', 'utf8' , (err, data) => {
+var FILE_PATH="/config.env/FILE_DATA";
+console.log("...voy a leer: "+FILE_PATH+";");
+fs.readFile( FILE_PATH , 'utf8' , (err, data) => {
 	  if (err) {
+		  console.log("...error leyenda: "+FILE_PATH+";");
 		      console.error(err)
 		      return
 		    }
